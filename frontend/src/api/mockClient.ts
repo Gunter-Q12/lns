2da723ed0ca7603043e696b5193db93cc58e3597
Gunter-R <2004vvr@gmail.com>
2026-04-24 00:01:17 +0300

    Add and use all demo rules

--- frontend/src/api/mockClient.ts
+++ frontend/src/api/mockClient.ts
@@ -5,10 +5,14 @@ import { LsnsResponse, LsnsResponseSchema } from '@/types/lsns';
 
 export async function mockFetchNft(_baseUrl: string, namespace?: string): Promise<NftResponse> {
   let data;
-  if (namespace === "/run/docker/netns/94ba6f52f019") {
-    data = await import('../testdata/nft/docker.json');
+  if (namespace === "/run/docker/netns/31beedc22845") {
+    data = await import('../testdata/nft/demo_docker0_nft.json');
+  } else if (namespace === "/run/docker/netns/0b4b9baa433a") {
+    data = await import('../testdata/nft/demo_docker1_nft.json');
+  } else if (namespace === "/run/docker/netns/6772672e60ad") {
+    data = await import('../testdata/nft/demo_docker2_nft.json');
   } else {
-    data = await import('../testdata/nft/long_chain.json');
+    data = await import('../testdata/nft/demo_host.json');
   }
   return NftResponseSchema.parse(data);
 }
@@ -16,19 +20,33 @@ export async function mockFetchNft(_baseUrl: string, namespace?: string): Promis
 export async function mockFetchRoute(_baseUrl: string, namespace?: string): Promise<IpResponse> {
   let route4Module, route6Module, rule4Module, rule6Module;
 
-  if (namespace === "/run/docker/netns/94ba6f52f019") {
+  if (namespace === "/run/docker/netns/31beedc22845") {
     [route4Module, route6Module, rule4Module, rule6Module] = await Promise.all([
-      import('../testdata/route/docker.json'),
-      import('../testdata/route/docker.json'),
-      import('../testdata/rule/docker4.json'),
-      import('../testdata/rule/docker6.json'),
+      import('../testdata/route/demo_docker0_4.json'),
+      import('../testdata/route/demo_docker0_6.json'),
+      import('../testdata/rule/demo_docker0_rule4.json'),
+      import('../testdata/rule/demo_docker0_rule6.json'),
+    ]);
+  } else if (namespace === "/run/docker/netns/0b4b9baa433a") {
+    [route4Module, route6Module, rule4Module, rule6Module] = await Promise.all([
+      import('../testdata/route/demo_docker1_4.json'),
+      import('../testdata/route/demo_docker1_6.json'),
+      import('../testdata/rule/demo_docker1_rule4.json'),
+      import('../testdata/rule/demo_docker1_rule6.json'),
+    ]);
+  } else if (namespace === "/run/docker/netns/6772672e60ad") {
+    [route4Module, route6Module, rule4Module, rule6Module] = await Promise.all([
+      import('../testdata/route/demo_docker2_4.json'),
+      import('../testdata/route/demo_docker2_6.json'),
+      import('../testdata/rule/demo_docker2_rule4.json'),
+      import('../testdata/rule/demo_docker2_rule6.json'),
     ]);
   } else {
     [route4Module, route6Module, rule4Module, rule6Module] = await Promise.all([
-      import('../testdata/route/route4.json'),
-      import('../testdata/route/route6.json'),
-      import('../testdata/rule/rule4.json'),
-      import('../testdata/rule/rule6.json'),
+      import('../testdata/route/demo_host4.json'),
+      import('../testdata/route/demo_host6.json'),
+      import('../testdata/rule/demo_host4.json'),
+      import('../testdata/rule/demo_host6.json'),
     ]);
   }
 
@@ -45,15 +63,19 @@ export async function mockFetchRoute(_baseUrl: string, namespace?: string): Prom
 
 export async function mockFetchAddr(_baseUrl: string, namespace?: string): Promise<AddrResponse> {
   let data;
-  if (namespace === "/run/docker/netns/94ba6f52f019") {
-    data = await import('../testdata/addr/docker.json');
+  if (namespace === "/run/docker/netns/31beedc22845") {
+    data = await import('../testdata/addr/demo_docker0_addr.json');
+  } else if (namespace === "/run/docker/netns/0b4b9baa433a") {
+    data = await import('../testdata/addr/demo_docker1_addr.json');
+  } else if (namespace === "/run/docker/netns/6772672e60ad") {
+    data = await import('../testdata/addr/demo_docker2_addr.json');
   } else {
-    data = await import('../testdata/addr/addr.json');
+    data = await import('../testdata/addr/demo_host.json');
   }
   return AddrResponseSchema.parse(data.default);
 }
 
 export async function mockFetchLsns(_baseUrl: string, _namespace?: string): Promise<LsnsResponse> {
-  const data = await import('../testdata/lsns/lsns.json');
+  const data = await import('../testdata/lsns/demo_host.json');
   return LsnsResponseSchema.parse(data.default);
 }
